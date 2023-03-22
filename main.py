2d64f674b0a6f54f12e7cedeb851d7216bef2f32
laben <laur104b@zbc.dk>
2023-03-22 10:41:12 +0100

    brændenekærlighed

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image, AsyncImage
+from kivy.uix.button import Button
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class OpskirftsApp(App):
+        Brændenekærlighed= {
+            'Ingredienser': [{'navn':'kartofler', 'mængde': 2, 'enhed':'kg'},
+                             {'navn':'løg','mængde':1, 'enhed':'antal' },
+                             {'navn':'mælk','mængde':1, 'enhed':'L'},
+                             {'navn':'salt','mængde':0.5, 'enhed':'tsk'},
+                             {'navn':'smør','mængde':10, 'enhed':'g'},
+                             {'navn':'bacon','mængde':1, 'enhed':'kg'}];
 
+            'Vejledning': [{'tekst':"Skræl kartoflerne", 'tid':240, 'skalere':True},
+                           {'tekst':"Hak kartofler i små firkanter", 'tid':240, 'skalere':True},
+                           {'tekst':"Kog kartoflerne", 'tid':600, 'skalere':False},
+                           {'tekst':"Hak løg i tern", 'tid':240, 'skalere':True},
+                           {'tekst':"Steg løg", 'tid':300, 'skalere':False},
+                           {'tekst':"Hæld vand fra kartofflerne", 'tid':60, 'skalere':False},
+                           {'tekst':"Hæld mælk salt og smør til kartoflerne imens du røre rundt", 'tid':120, 'skalere':False},
+                           {'tekst':"Pisk indtil tykt", 'tid':240, 'skalere':True},
+                           {'tekst':"Steg bacon", 'tid':360, 'skalere':False}];
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        };
